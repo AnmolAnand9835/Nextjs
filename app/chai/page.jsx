7c420eb328473ec9a69eb48a3af2889f2ef81060
Sudo_why <anmolcollection123@gmail.com>
2026-07-16 01:10:46 +0530

    just add first commit

---
+++ app/chai/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Chai = () => {
+  return (
+    <div>
+      Chai aur code 👀
+      <Button variant="outline">sulululu</Button>
+    </div>
+  );
+};
+
+export default Chai;
---
+++ model/User.ts
@@ -0,0 +1,61 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createrdAt: Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    require: true,
+  },
+  createrdAt: {
+    type: Date,
+    require: true,
+    default: Date.now,
+  },
+});
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCodeExpiry: Date;
+  isverified: boolean;
+  isAcceptingMessage: boolean;
+  message: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    require: [true, "username is required"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    require: [true, "email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "please use a valid email address"],
+  },
+  password: {
+    type: String,
+    require: [true, "password is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    require: [true, "code is required"],
+  },
+  isverified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  message: [MessageSchema]
+});
+
+const UserModel = (mongoose.model.User as mongoose.Model<User>)
